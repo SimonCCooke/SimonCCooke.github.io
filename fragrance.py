072d80ba39dcfd57f53709a5cb36608f3ff7fbe7
Simon Cooke <uf.scooke@gmail.com>
2017-08-01 22:46:42 -0400

    Update

--- fragrance.py
+++ fragrance.py
@@ -13,35 +13,13 @@ def main():
     print("Opened database successfully.")
     cursor = conn.cursor()
     cursor.execute('''create table if not exists fragrance
-        (id int primary key not null,
+        (id integer primary key,
         name text not null,
         brand text not null,
         rating text,
         reviews text);''')
-    conn.commit()
+
     print("Table created successfully.")
-    r = requests.get("http://www.basenotes.net/ID26126902.html")
-
-    data = r.content
-
-    soup = BeautifulSoup(data)
-
-    cursor.execute(
-        '''insert into fragrance(id, name, brand, rating, reviews)
-        values(?,?,?,?,?)''',
-        (
-            "1",
-            soup.find(itemprop="name").text,
-            soup.find(itemprop="brand manufacturer").text,
-            soup.find(itemprop="ratingValue")["content"],
-            soup.find(itemprop="reviewCount").text))
-
-    cursor.execute('''select * from fragrance''')
-    allrows = cursor.fetchall()
-    for row in allrows:
-        print('{0} : {1} {2} {3} {4}'.format(
-                row[0], row[1], row[2], row[3], row[4]))
-    conn.close()
 
     r = requests.get("http://www.basenotes.net/brand/")
 
@@ -64,13 +42,11 @@ def main():
 
     data1 = soup.find_all("h3")
     for single in data1:
-        print(single.contents[0].attrs["href"])
-#    data2 = data1.contents[0].attrs["href"]
-#    print(data2)
-#   for i in range(20):
-    #    data1 = data1.find_next("h3")
-    #    data2 = data1.contents[0].attrs["href"]
-    #    print(data2)
+        r = requests.get(single.contents[0].attrs["href"])
+        data = r.content
+        soup = BeautifulSoup(data)
+        cursor.execute('''insert into fragrance(name, brand, rating, reviews)
+                values(?,?,?,?)''', ("2", "3", "4", "5"))
 
 
 if __name__ == '__main__':
