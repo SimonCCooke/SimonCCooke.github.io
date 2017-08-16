b2720b6c834c2e56185cf688e69f568527841960
Simon Cooke <uf.scooke@gmail.com>
2017-08-16 11:32:54 -0400

    Fragrance Notes initialize

---
+++ fragrancenotes.py
@@ -0,0 +1,14 @@
+import requests
+from bs4 import BeautifulSoup
+import time
+import sqlite3
+import pandas as pd
+from pandas import ExcelWriter
+from pandas import ExcelFile
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+    time.sleep(60)
