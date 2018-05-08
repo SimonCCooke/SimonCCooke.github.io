b07c91d62fe03b9a27aa41b6b8adbc0f9a46f267
Simon Cooke <uf.scooke@gmail.com>
2018-05-08 13:41:24 -0400

    day 2

--- javascript/colorGame.js
+++ javascript/colorGame.js
@@ -2,4 +2,28 @@ var colors = []
 
 var hardColors = []
 
-var easyColors = []
+var easyColors =          ["IndianRed","LightCoral","Salmon","DarkSalmon","LightSalmon","Crimson","Red","FireBrick","DarkRed","Pink","LightPink","HotPink","DeepPink","MediumVioletRed","PaleVioletRed","LightSalmon","Coral","Tomato","OrangeRed","DarkOrange","Orange","Gold","Yellow","LightYellow","LemonChiffon","LightGoldenrodYellow","PapayaWhip","Moccasin","PeachPuff","PaleGoldenrod","Khaki","DarkKhaki","Lavender","Thistle","Plum","Violet","Orchid","Fuchsia","Magenta","MediumOrchid","MediumPurple","Amethyst","BlueViolet","DarkViolet","DarkOrchid","DarkMagenta","Purple","Indigo","SlateBlue","DarkSlateBlue","MediumSlateBlue","GreenYellow","Chartreuse","LawnGreen","Lime","LimeGreen","PaleGreen","LightGreen","MediumSpringGreen","SpringGreen","MediumSeaGreen","SeaGreen","ForestGreen","Green","DarkGreen","YellowGreen","OliveDrab","Olive","DarkOliveGreen","MediumAquamarine","DarkSeaGreen","LightSeaGreen","DarkCyan","Teal","Aqua","Cyan","LightCyan","PaleTurquoise","Aquamarine","Turquoise","MediumTurquoise","DarkTurquoise","CadetBlue","SteelBlue","LightSteelBlue","PowderBlue","LightBlue","SkyBlue","LightSkyBlue","DeepSkyBlue","DodgerBlue","CornflowerBlue","MediumSlateBlue","RoyalBlue","Blue","MediumBlue","DarkBlue","Navy","MidnightBlue","Cornsilk","BlanchedAlmond","Bisque","NavajoWhite","Wheat","BurlyWood","Tan","RosyBrown","SandyBrown","Goldenrod","DarkGoldenrod","Peru","Chocolate","SaddleBrown","Sienna","Brown","Maroon","White","Snow","Honeydew","MintCream","Azure","AliceBlue","GhostWhite","WhiteSmoke","Seashell","Beige","OldLace","FloralWhite","Ivory","AntiqueWhite","Linen","LavenderBlush","MistyRose","Gainsboro","LightGrey","Silver","DarkGray","Gray","DimGray","LightSlateGray","SlateGray","DarkSlateGray","Black"]
+
+var squares = document.querySelectorAll(".colorGameSquare");
+var ans =  easyColors[3].toLowerCase();
+var colorDisplay = document.getElementById("colorDisplay");
+
+colorDisplay.textContent = ans;
+
+
+$(document).ready(function(){
+  alert(document.body.style.backgroundColor);
+for(var i = 0; i<squares.length; i++){
+  squares[i].style.backgroundColor = easyColors[i];
+
+  squares[i].addEventListener("click", function(){
+    var clickedColor = this.style.backgroundColor.toLowerCase();
+    if(clickedColor === ans){
+      alert("Correct");
+    } else {
+      alert("Wrong");
+      this.style.backgroundColor = "#3AAFA9";
+    }
+  });
+};
+});
